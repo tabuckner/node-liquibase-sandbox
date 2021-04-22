0fcb4b6fc0ac4d725ee77618b19871e69a734902
Taylor Buckner <taylora.buckner@gmail.com>
2021-04-22 12:56:56 -0500

    branch showing how to use @pd

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ const myConfig: LiquibaseConfig = {
   url: 'jdbc:postgresql://localhost:5432/node_liquibase_testing',
   username: 'yourusername',
   password: 'yoursecurepassword',
+  liquibase: '/usr/local/opt/liquibase/liquibase',
+  classpath: './drivers/postgresql-42.2.8.jar'
 }
 const inst = new Liquibase(myConfig);
 
